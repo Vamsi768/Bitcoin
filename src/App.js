8d0e34e9cf56d04803486f2695be110d27b6ff46
VENKAT NAVEEN MEDARAMETLA <41520213+VenkatNaveenM@users.noreply.github.com>
2021-08-04 19:24:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./tailwind.css";
+import Market from "./Components/Market/Market";
+import Nav from "./Components/Nav/Nav";
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Market />
+    </div>
+  );
+}
+
+export default App;
