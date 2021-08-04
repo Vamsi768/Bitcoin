fbb4dd36203be30386bbd9d208338cef85c32190
VENKAT NAVEEN MEDARAMETLA <41520213+VenkatNaveenM@users.noreply.github.com>
2021-08-04 19:26:02 +0530

    Add files via upload

---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Nav() {
+  return (
+    <nav className="nav-wrapper blue darken-3">
+      <div className="container">
+        <h1 className="brand-logo m-4">Bitcoin</h1>
+      </div>
+    </nav>
+  );
+}
